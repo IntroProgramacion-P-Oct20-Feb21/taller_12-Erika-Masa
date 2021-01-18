9800400176d2b66b1c2672f18f6f59a2033ddc0c
Erika Masa <gisellamasa123@gmail.com>
2021-01-17 22:09:12 -0500

    solucion del taller 12

--- Ejercicios/src/ejercicios/Problema03.java
+++ Ejercicios/src/ejercicios/Problema03.java
@@ -16,6 +16,30 @@ public class Problema03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+     double valor1;
+        double valor2;
+        double[] datos = {100, 90, 80, 70, 60};
+        double[] datos2 = {200, 190, 180, 170, 160};
+        double[] suma = new double[5];
+        for (int i = 0; i < 5; i++) {
+            valor1 = datos[i];
+            valor2 = datos2[i];
+            suma[i] = datosSuma(valor1, valor2);
+            System.out.println(suma[i]);
+        }
+
     }
-    
+
+    public static double datosSuma(double valor1, double valor2) {
+        if (valor2 == 0) {
+            return valor1;
+        } else {
+            if (valor1 == 0) {
+                return valor2;
+            } else {
+                return 1 + datosSuma(valor1, valor2 - 1);
+            }
+        }
+
+    }
 }
--- Ejercicios/src/ejercicios/Problema04.java
+++ Ejercicios/src/ejercicios/Problema04.java
@@ -16,6 +16,48 @@ public class Problema04 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+       double base;
+        double potencia;
+        double[] bases = {100, 90, 80, 70, 60};
+        double[] potencias = {2, 3, 2, 2, 1};
+        double[] resultado = new double[5];
+        String cadenaFinal = " ";
+        
+        for (int i = 0; i < 5 ; i++) {
+               base = bases[i];
+            potencia = potencias[i];
+            resultado[i] = obtenerPotencia(bases[i], potencias[i]);
+            cadenaFinal = String.format("%sEl resultado de la potencia: %.2f\n",cadenaFinal, resultado[i]);
+        }
+        
+        cadenaFinal = String.format("%s La suma de todas las potencias es : \n%.2f\n",
+                cadenaFinal, obtenerSuma(resultado));
+        System.out.printf("%s", cadenaFinal);
     
+        
+    }
+
+    public static double obtenerPotencia(double base, double potencia) {
+
+        if (potencia == 1) {
+            return base;
+        } else {
+            return base * obtenerPotencia(base, potencia - 1);
+        }
+    }
+     public static double obtenerSuma(double[] arreglo) {
+        double suma = 0;
+        for (int i = 0; i < 5; i++){
+            suma = suma + arreglo[i];
+        }
+        return suma;
+    }
+
 }
+
+
+
+    
+    
+    
+    
